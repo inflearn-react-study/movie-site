b70f286f14df44a1ebed9ada6a6700ac1d6517ed
jslee <dlwhdtjd098@naver.com>
2021-06-20 19:37:59 +0900

    MainImage.js Commons dir 로 이동

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 // import {FaCode} from "react-icons/fa";
 import {API_KEY, API_URL, IMAGE_BASE_URL} from "../../Config";
-import MainImage from "./Sections/MainImage";
+import MainImage from "../commons/MainImage";
 import GridCards from "../commons/GridCards";
 import {Row} from 'antd';
 
