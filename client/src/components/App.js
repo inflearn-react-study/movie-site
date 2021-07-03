499d67dde495a3b0beb114780df9cad07296de66
jslee <dlwhdtjd098@naver.com>
2021-07-03 22:21:21 +0900

    fix #3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ function App() {
                     <Route exact path="/" component={Auth(LandingPage, null)}/>
                     <Route exact path="/login" component={Auth(LoginPage, false)}/>
                     <Route exact path="/register" component={Auth(RegisterPage, false)}/>
-                    <Route exact path="/movie/:movieId" component={Auth(MovieDetail, false)}/>
+                    <Route exact path="/movie/:movieId" component={Auth(MovieDetail, true)}/>
                 </Switch>
             </div>
             <Footer/>
